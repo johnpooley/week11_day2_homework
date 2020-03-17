33ecf9a75f4be135e01848c93467dc56fd0ecda3
John <user@johnpoosmacbook.home>
2020-03-17 22:25:02 +0000

    homework mvp done. trying to search by author but having issues

---
+++ src/main/java/Library.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+
+public class Library {
+    public static void main(String[] args) {
+        HashMap<String,String,String> library = new HashMap<String, String, String>();
+    }
+}
